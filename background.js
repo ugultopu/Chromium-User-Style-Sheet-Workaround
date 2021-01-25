09f37df1c7da1e3d8e7c48d5e3359c879031af37
Utku Gultopu <ugultopu@gmail.com>
2021-01-25 16:29:26 -0500

    Don't inject the style on localhost
    Maybe not too necessary but might be helpful for testing local websites
    without disabling the extension.

--- background.js
+++ background.js
@@ -1,16 +1,14 @@
+const localhostPattern = new RegExp('^https?://localhost[/:]', 'i');
+
 chrome.webNavigation.onCommitted.addListener(
-  ({tabId}) => {
+  ({tabId, url}) => {
+    if(localhostPattern.test(url)) return;
+
     chrome.tabs.insertCSS(
       tabId,
       {
         file: 'default.css',
         cssOrigin: 'user',
-        // Possible values of 'runAt' are 'document_start',
-        // 'document_end' and 'document_idle', with 'document_idle'
-        // being the default. However, both 'document_end' and
-        // 'document_idle' causes a flash of the website content before
-        // the user style takes effect. Hence, the only right way is to
-        // give 'document_start' as the value of 'runAt'.
         runAt: 'document_start'
       }
     );
