9dbbb7594b7f22cddd13bf97592058483059f979
Utku Gultopu <ugultopu@gmail.com>
2021-01-24 19:49:39 -0500

    Find the cause of style flash and fix it
    Before I gave 'document_start' as the value of the 'runAt' argument,
    there was a flash of website styles before my styles were taking effect.
    After I gave 'document_start' as the value of the 'runAt' argument, this
    works properly.

--- background.js
+++ background.js
@@ -4,7 +4,14 @@ chrome.webNavigation.onCommitted.addListener(
       tabId,
       {
         file: 'default.css',
-        cssOrigin: 'user'
+        cssOrigin: 'user',
+        // Possible values of 'runAt' are 'document_start',
+        // 'document_end' and 'document_idle', with 'document_idle'
+        // being the default. However, both 'document_end' and
+        // 'document_idle' causes a flash of the website content before
+        // the user style takes effect. Hence, the only right way is to
+        // give 'document_start' as the value of 'runAt'.
+        runAt: 'document_start'
       }
     );
   },
